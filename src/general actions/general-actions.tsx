d4bd83b9999cad3b05fd512dc403e640cd666872
dbaluku <juniorderrick18@gmail.com>
2019-06-20 18:05:55 +0300

    delete patient works

---
+++ src/general actions/general-actions.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { css } from "@emotion/core";
+import Parcel from "single-spa-react/parcel";
+import deletePatientParcel from "./delete-patient.parcel";
+
+// const generalActionsParcels = [
+//   () => import("./delete-patient.parcel").then(m => m.default),
+// ];
+
+export default function GeneralActions(props: GeneralActionsProps) {
+  return (
+    <div
+      css={css`
+        margin: 76px auto 0 auto;
+        width: 500px;
+      `}
+    >
+      <Parcel
+        config={deletePatientParcel}
+        patientUuid={props.match.params.patientUuid}
+      />
+    </div>
+  );
+}
+
+type GeneralActionsProps = {
+  match: {
+    params: {
+      patientUuid: string;
+    };
+  };
+};
--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import GeneralActions from "./general actions/general-actions";
+import { BrowserRouter, Route } from "react-router-dom";
 
 export default function Root(props: RootProps) {
-  return <div className="test">Hackathon seed is working!</div>;
+  return (
+    <BrowserRouter basename="/openmrs/spa">
+      <Route
+        path="/patient-dashboard/:patientUuid"
+        component={GeneralActions}
+      />
+    </BrowserRouter>
+  );
 }
 
 type RootProps = {};
